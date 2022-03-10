9f59dbfea313c123aa5b4aec562e74017e1b129c
Haiden Campos <iambob661@gmail.com>
2022-03-10 00:04:15 -0600

    Scuffed together a very basic 2D renderer
    just a start

--- DevGround/Source/Sandbox2D.cpp
+++ DevGround/Source/Sandbox2D.cpp
@@ -13,35 +13,8 @@ Sandbox2D::Sandbox2D()
 void Sandbox2D::OnAttach()
 {
 	
-	//-------------------------------------------------------------------------------------------------
-	m_SquareVA = Hazel::VertexArray::Create();
 
-	float sqVertices[3 * 4] =             
-	{
-		-0.5f, -0.5f, 0.f,
-		 0.5f, -0.5f, 0.f,
-		 0.5f,  0.5f, 0.f,
-		-0.5f,  0.5f, 0.f
-	};
-	Hazel::Ref<Hazel::VertexBuffer> squareVB;
-	squareVB.reset(Hazel::VertexBuffer::Create(sqVertices, sizeof(sqVertices)));
-
-
-	Hazel::BufferLayout sqLayout =
-	{
-		{Hazel::ShaderDataType::Float3, "a_Posistion" }
-	};
-	squareVB->SetLayout(sqLayout);
-	m_SquareVA->AddVertexBuffer(squareVB);
-
-	uint32_t sqIndices[6] = { 0,1,2,2,3,0 };
-	Hazel::Ref<Hazel::IndexBuffer> squareIB;
-	squareIB.reset(Hazel::IndexBuffer::Create(sqIndices, sizeof(sqIndices) / sizeof(uint32_t)));
-	m_SquareVA->SetIndexBuffer(squareIB);
-	//-------------------------------------------------------------------------------------------------
 	
-
-	m_FlatColorShader = Hazel::Shader::Upload("assets/shaders/FlatColorShader.glsl");
 }
 
 void Sandbox2D::OnDetach()
@@ -58,18 +31,18 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 	Hazel::RenderCommand::SetClearColor({ 0.04f, 0.04f, 0.04f, 1 });
 	Hazel::RenderCommand::Clear();
 
-	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
+	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	
-
-
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-
-	Hazel::Renderer::Sumbit(m_SquareVA, m_FlatColorShader, glm::scale(glm::mat4(1.f), glm::vec3(1.5f)));
+	Hazel::Renderer2D::DrawQuad({ 0.f, 0.f }, { 1.f, 1.f }, { 0.8f, 0.67f, 0.12f, 0.8f });
 
 
-	Hazel::Renderer::EndScene();
+
+
+	Hazel::Renderer2D::EndScene();
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+	//
+	//Hazel::Renderer::Sumbit(m_SquareVA, m_FlatColorShader, glm::scale(glm::mat4(1.f), glm::vec3(1.5f)));
 }
 
 void Sandbox2D::OnImGuiRender()
---
+++ Hazel/Source/Hazel/Renderer/Renderer2D.cpp
@@ -0,0 +1,83 @@
+#include "hzpch.h"
+#include "Renderer2D.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "RenderCommand.h"
+
+//temp
+#include "Platform/opengl/OpenGLShader.h"
+
+namespace Hazel
+{
+	struct Renderer2Ddata
+	{
+		Ref<VertexArray> QuadVertexArray;
+		Ref<Shader> m_FlatColorShader;
+	};
+
+	static Renderer2Ddata* s_Data;
+
+	void Renderer2D::Init()
+	{
+		s_Data = new Renderer2Ddata();
+
+		s_Data->QuadVertexArray = VertexArray::Create();
+
+		float sqVertices[3 * 4] =
+		{
+			-0.5f, -0.5f, 0.f,
+			 0.5f, -0.5f, 0.f,
+			 0.5f,  0.5f, 0.f,
+			-0.5f,  0.5f, 0.f
+		};
+		Ref<VertexBuffer> squareVB;
+		squareVB.reset(VertexBuffer::Create(sqVertices, sizeof(sqVertices)));
+
+
+		BufferLayout sqLayout =
+		{
+			{ShaderDataType::Float3, "a_Posistion" }
+		};
+		squareVB->SetLayout(sqLayout);
+		s_Data->QuadVertexArray->AddVertexBuffer(squareVB);
+
+		uint32_t sqIndices[6] = { 0,1,2,2,3,0 };
+		Ref<IndexBuffer> squareIB;
+		squareIB.reset(IndexBuffer::Create(sqIndices, sizeof(sqIndices) / sizeof(uint32_t)));
+		s_Data->QuadVertexArray->SetIndexBuffer(squareIB);
+
+
+
+		s_Data->m_FlatColorShader = Shader::Upload("assets/shaders/FlatColorShader.glsl");
+	}
+
+	void Renderer2D::Shutdown()
+	{
+		delete s_Data;
+	}
+
+	void Renderer2D::BeginScene(const OrthographicCamera& camera)
+	{
+		std::dynamic_pointer_cast<OpenGLShader>(s_Data->m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<OpenGLShader>(s_Data->m_FlatColorShader)->UploadUniformMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
+		std::dynamic_pointer_cast<OpenGLShader>(s_Data->m_FlatColorShader)->UploadUniformMat4("u_Transform", glm::mat4(1.f));
+	}
+
+	void Renderer2D::EndScene()
+	{
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
+	{
+		DrawQuad({ position.x, position.y, 0.f }, size, color);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
+	{
+		std::dynamic_pointer_cast<OpenGLShader>(s_Data->m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<OpenGLShader>(s_Data->m_FlatColorShader)->UploadUniformFloat4("u_Color", color);
+
+		s_Data->QuadVertexArray->Bind();
+		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
+	}
+}
