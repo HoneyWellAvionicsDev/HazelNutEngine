d7980113bf00491bd0b7631240589b7eebb0ef71
Haiden Campos <iambob661@gmail.com>
2022-03-15 18:29:12 -0500

    Refactored Input class

--- Hazel/Source/Hazel/Core/Input.h
+++ Hazel/Source/Hazel/Core/Input.h
@@ -4,23 +4,14 @@
 
 namespace Hazel
 {
-	class HAZEL_API Input
+	class Input
 	{
 	public:
-		//when we call this function we pass in the keycode to check, return an instance of this class and from it call IsKeyPressedImpl which then calls its impl in windowsInput
-		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
-		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+		static bool IsKeyPressed(int keycode);
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	
-	protected:
-		virtual bool IsKeyPressedImpl(int keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(int button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-	private:
-		static Input* s_Instance;
 	};
 }
--- Hazel/Source/Platform/Windows/WindowsInput.cpp
+++ Hazel/Source/Platform/Windows/WindowsInput.cpp
@@ -1,42 +1,40 @@
 #include "hzpch.h"
-#include "WindowsInput.h"
+#include "Hazel/Core/Input.h"
 #include "Hazel/Core/Application.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Hazel
 {
-	Input* Input::s_Instance = new WindowsInput();
-
-	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	bool Input::IsKeyPressed(int keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());            //gets our application window as a void pointer and casts it into a GLFWwindow pointer
 		auto state = glfwGetKey(window, keycode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
-	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	bool Input::IsMouseButtonPressed(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 
 	}
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
 		glfwGetCursorPos(window, &xpos, &ypos);
 		return std::pair<float, float>(xpos, ypos);
 	}
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();               // [names the values this func returns]
+		auto [x, y] = GetMousePosition();               // [names the values this func returns]
 		return x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return y;
 	}
 }
