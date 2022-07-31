a67d1f59976cacbec3cbd89cffd52296ce146d75
Haiden Campos <iambob661@gmail.com>
2022-07-31 00:35:52 -0500

    FIxed viewport bounds being offset on the y asix

--- Hazelnut/Source/HazelEditorLayer.cpp
+++ Hazelnut/Source/HazelEditorLayer.cpp
@@ -137,7 +137,7 @@ namespace Hazel
         mx -= m_ViewportBounds[0].x;
         my -= m_ViewportBounds[0].y;
         glm::vec2 viewportSize = m_ViewportBounds[1] - m_ViewportBounds[0];
-        my = viewportSize.y = my;
+        my = viewportSize.y - my;
         int mouseX = (int)mx;
         int mouseY = (int)my;
         if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y) //this happens every frame, maybe make it happen on mouse click
@@ -278,7 +278,11 @@ namespace Hazel
     
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0,0 });
         ImGui::Begin("Viewport");
-        auto viewportOffset = ImGui::GetCursorPos(); //includes tab bar
+        auto viewportMinRegion = ImGui::GetWindowContentRegionMin();
+        auto viewportMaxRegion = ImGui::GetWindowContentRegionMax();
+        auto viewportOffset = ImGui::GetWindowPos(); //includes tab bar
+        m_ViewportBounds[1] = { viewportMaxRegion.x + viewportOffset.x, viewportMaxRegion.y + viewportOffset.y };
+        m_ViewportBounds[0] = { viewportMinRegion.x + viewportOffset.x, viewportMinRegion.y + viewportOffset.y };
 
         m_ViewportFocused = ImGui::IsWindowFocused();
         m_ViewportHovered = ImGui::IsWindowHovered();                                            // either control key should prevent imgui blocking events
@@ -288,8 +292,8 @@ namespace Hazel
         m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
   
         uint32_t textureID = m_FrameBuffer->GetColorAttachmentRendererID();
-        ImGui::Image((void*)textureID, ImVec2{m_ViewportSize.x, m_ViewportSize.y}, ImVec2{0,1}, ImVec2{1,0});
-
+        ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0,1 }, ImVec2{ 1,0 });
+#if 0
         auto windowSize = ImGui::GetWindowSize();
         ImVec2 minBound = ImGui::GetWindowPos();
         minBound.x += viewportOffset.x;
